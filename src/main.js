cb31b7185b3ab4119811013bec09dd1337b306a2
Tomas Salgado <tomassalgadodev@gmail.com>
2022-12-06 16:07:28 -0700

    feat: Add the ability to switch between the pages

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
 // Create variables targetting the relevant DOM elements here 👇
 
-
+// Cover Variables
 var coverTitle = document.querySelector('.cover-title');
 var tagline1 = document.querySelector('.tagline-1');
 var tagline2 = document.querySelector('.tagline-2');
 var image = document.querySelector('.cover-image');
+
+// Buttons
+var homeButton = document.querySelector('.home-button');
+var saveButton = document.querySelector('.save-cover-button');
+var viewSavedButton = document.querySelector('.view-saved-button');
 var showNewCoverButton = document.querySelector('.random-cover-button');
+var makeCustomCoverButton = document.querySelector('.make-new-button');
+
+// Page Sections
+var homeSection = document.querySelector('.home-view');
+var savedSection = document.querySelector('.saved-view');
+var customSection = document.querySelector('.form-view');
 
 console.log(coverTitle);
 
@@ -18,6 +29,12 @@ var currentCover;
 // Add your event listeners here 👇
 
 showNewCoverButton.addEventListener('click', displayRandomCover);
+
+makeCustomCoverButton.addEventListener('click', showCustomPage);
+
+viewSavedButton.addEventListener('click', showSavedPage);
+
+homeButton.addEventListener('click', showHomePage);
 
 // Create your event handlers and other functions here 👇
 
@@ -40,6 +57,39 @@ function displayRandomCover() {
   tagline2.innerText = newCover.tagline2;
 }
 
+function showCustomPage() {
+  homeButton.classList.remove('hidden');
+  saveButton.classList.add('hidden');
+  showNewCoverButton.classList.add('hidden');
+  homeSection.classList.add('hidden');
+  savedSection.classList.add('hidden')
+  customSection.classList.remove('hidden');
+}
+
+function showSavedPage() {
+  homeButton.classList.remove('hidden');
+  saveButton.classList.add('hidden');
+  showNewCoverButton.classList.add('hidden');
+  homeSection.classList.add('hidden');
+  customSection.classList.add('hidden');
+  savedSection.classList.remove('hidden');
+}
+
+function showHomePage() {
+  homeButton.classList.add('hidden');
+  saveButton.classList.remove('hidden');
+  showNewCoverButton.classList.remove('hidden');
+  customSection.classList.add('hidden');
+  savedSection.classList.add('hidden');
+  homeSection.classList.remove('hidden');
+}
+  
+
+// We've provided one function to get you started
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
 // function createNewCover() {
 //   var randomTitle = titles[getRandomIndex(titles)];
 //   var randomTag1 = descriptors[getRandomIndex(descriptors)];
@@ -50,8 +100,3 @@ function displayRandomCover() {
 //   tagline1.innerText = randomTag1;
 //   tagline2.innerText = randomTag2;
 // }
-
-// We've provided one function to get you started
-function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
-}
