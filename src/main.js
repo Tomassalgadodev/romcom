39f0fdf5ad04a6e2ea606f62454b009982f916a6
Tomas Salgado <tomassalgadodev@gmail.com>
2022-12-07 12:02:51 -0700

    feat: Allow covers to be saved and viewed in the saved tab

--- src/main.js
+++ src/main.js
@@ -18,6 +18,7 @@ var makeUserBookButton = document.querySelector('.create-new-book-button');
 var homeSection = document.querySelector('.home-view');
 var savedSection = document.querySelector('.saved-view');
 var customSection = document.querySelector('.form-view');
+var savedCoversSection = document.querySelector('.saved-covers-section');
 
 // Input Fields
 
@@ -28,9 +29,7 @@ var userTagline2 = document.querySelector('.user-desc2');
 
 
 // We've provided a few variables below
-var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-];
+var savedCovers = [];
 var currentCover;
 
 // Add your event listeners here 👇
@@ -45,7 +44,7 @@ homeButton.addEventListener('click', showHomePage);
 
 makeUserBookButton.addEventListener('click', makeUserBook);
 
-
+saveButton.addEventListener('click', saveCover);
 
 // Create your event handlers and other functions here 👇
 
@@ -61,11 +60,11 @@ function createRandomCover() {
 }
 
 function displayRandomCover() {
-  var newCover = createRandomCover();
-  image.setAttribute('src', newCover.cover);
-  coverTitle.innerText = newCover.title;
-  tagline1.innerText = newCover.tagline1;
-  tagline2.innerText = newCover.tagline2;
+  currentCover = createRandomCover();
+  image.setAttribute('src', currentCover.cover);
+  coverTitle.innerText = currentCover.title;
+  tagline1.innerText = currentCover.tagline1;
+  tagline2.innerText = currentCover.tagline2;
 }
 
 function showCustomPage() {
@@ -85,6 +84,7 @@ function showSavedPage() {
   homeSection.classList.add('hidden');
   customSection.classList.add('hidden');
   savedSection.classList.remove('hidden');
+  displaySavedCovers();
 }
 
 function showHomePage() {
@@ -97,16 +97,40 @@ function showHomePage() {
 }
 
 function makeUserBook() {
-  userBook = new Cover(userCover.value, userTitle.value, userTagline1.value, userTagline2.value);
-  image.setAttribute('src', userBook.cover);
-  coverTitle.innerText = userBook.title;
-  tagline1.innerText = userBook.tagline1;
-  tagline2.innerText = userBook.tagline2;
-  covers.push(userBook.cover);
-  titles.push(userBook.title);
-  descriptors.push(userBook.tagline1, userBook.tagline2);
-  console.log(covers);
+  currentCover = new Cover(userCover.value, userTitle.value, userTagline1.value, userTagline2.value);
+  image.setAttribute('src', currentCover.cover);
+  coverTitle.innerText = currentCover.title;
+  tagline1.innerText = currentCover.tagline1;
+  tagline2.innerText = currentCover.tagline2;
+  covers.push(currentCover.cover);
+  titles.push(currentCover.title);
+  descriptors.push(currentCover.tagline1, currentCover.tagline2);
   showHomePage();
+}
+
+function displaySavedCovers() {
+  var concattedCovers = '';
+  for (var i = 0; i < savedCovers.length; i++) {
+    concattedCovers = concattedCovers + `    
+    <section class="mini-cover">
+      <img class="cover-image" src="${savedCovers[i].cover}">
+      <h2 class="cover-title">${savedCovers[i].title}</h2>
+      <h3 class="tagline">A tale of <span class="tagline-1">${savedCovers[i].tagline1}</span> and <span class="tagline-2">${savedCovers[i].tagline2}</span></h3>
+      <img class="price-tag" src="./assets/price.png">
+      <img class="overlay" src="./assets/overlay.png">
+    </section>
+    `
+  }
+  savedCoversSection.innerHTML = concattedCovers;  
+}
+
+function saveCover() {
+  for (var i = 0; i < savedCovers.length; i++) {
+    if (currentCover.id === savedCovers[i].id) {
+      return;
+    }
+  }
+  savedCovers.push(currentCover);
 }
   
 
